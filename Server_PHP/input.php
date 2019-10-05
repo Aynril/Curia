cba726e68f57db9d1f3b5e258f3b115f6f8223cb
Aynril <fabian.jaser@gmail.com>
2019-10-05 18:52:29 +0200

    added file

--- Server_PHP/input.php
+++ Server_PHP/input.php
@@ -1,5 +1,13 @@
 <?php
 
+require("database.php");
 // This file enters the submitted data to the db
 
-$connection = include "connect.php";
+$begriff = $_POST['begriff'];
+$definition = $_POST['definition'];
+$aktiv = 1;
+$zuletzt_aktualisiert = $_POST['zuletzt_aktualisiert'];
+$autor = $_POST['autor'];
+$kategorie = $_POST['kategorie'];
+
+echo $begriff.$definition.$aktiv.$zuletzt_aktualisiert.$autor.$kategorie;
