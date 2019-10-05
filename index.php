f4c163bf65527e02ac886e1f7f09c233af08952e
Clemens <kontakt@clemensrau.de>
2019-10-05 20:59:51 +0200

    test

--- index.php
+++ index.php
@@ -12,6 +12,23 @@
 </head>
 <body>
 
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+      <a class="navbar-brand" href="#">Curia</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Startseite <span class="sr-only"></span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Admin Login</a>
+          </li>
+       
+        </ul>
+      </div>
+    </nav>
 
     <!-- Begin page content -->
     <main role="main" class="container">
