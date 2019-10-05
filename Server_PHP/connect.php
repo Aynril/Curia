79269abc1696821cae832b40e908d81c0d2ba24b
zoxuyu <maus@paulstrobach.de>
2019-10-05 14:43:54 +0200

    Also add first line ;)

--- Server_PHP/connect.php
+++ Server_PHP/connect.php
@@ -2,6 +2,7 @@
 
 // This file creates a PDO connection
 
+$connection = new PDO('mysql:host=localhost;dbname=curia;charset=UTF8', "jhberlin19", "jhberlin19");
 $connection->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
 return $connection;
