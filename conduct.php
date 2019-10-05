d2a47cfa2121ddb20f2037f2ea396f980e09acab
zoxuyu <maus@paulstrobach.de>
2019-10-05 22:13:37 +0200

    Add CoC

---
+++ conduct.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" href="Logo.ico">
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>CURIA - Endlich mitreden bei politischen Diskussionen</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!-- sweet alerts -->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+ <!-- Brand / Logo-->
+ <a class="navbar-brand" href="#">CURIA</a>
+
+</nav>
+
+   <!-- Begin page content -->
+   <main role="main" class="container">
+     <div class="jumbotron">
+         <h1>Code of Conduct</h1>
+     </div>
+
+     <p class="lead">Wir haben uns diesen <em>Code of Conduct</em> gegeben, damit wir alle freundlich und freundschaftlich gemeinsam an der Verbreitung politischen Wissens arbeiten können.</p>
+
+     <h2>Auf wen trifft dieser CoC zu?</h2>
+     <p>Dieser Code of Conduct trifft auf alle Menschen (und natürlich auch Bots), die sich an unserem Projekt beteiligen. Dies beinhaltet alle, die Begriffe hinzufügen und definieren, und alle, die an der Quelltext-Entwicklung unseres Projekts mitarbeiten.</p>
+     <h2>Sei freundlich!</h2>
+     <p>Immer, wenn du etwas zu CURIA beiträgst, bemühe dich es freundlich zu formulieren. Wenn du etwas nicht sagen kannst, ohne irgendjemanden zu beleidigen oder anzugreifen, dann lass es lieber.</p>
+     <p>Diese Regel sollte eine gute Grundlage bieten, dennoch sind die folgenden Sachen noch einmal explizit verboten:</p>
+     <ul>
+         <li>Diskriminierung jeder Art, ob wegen politischen Gründen, aus Gründen der Identität oder anderem</li>
+         <li>Aufruf zu, Ankündigung von oder Glorifizierung von jeglicher Art von Gewalt, Hass oder Verbrechen</li>
+         <li>Mobbing oder andere gezielte Angriffe gegen einzelne Menschen</li>
+     </ul>
+     <p>Auf keinen Fall sollen diese Regeln so ausgelegt werden, dass die Auslegung dem Geist dieser Regeln widerspricht. Dies gilt insbesondere für zu enge, "textnahe" Auslegungen.</p>
+     <h2>Sei sachlich!</h2>
+     <p>Bei CURIA bemühen wir uns um Sachlichkeit und politische Neutralität. </p>
+     <p>Insbesondere ist es verboten, politisch geladene oder einseitige Stellungnahmen in den Beschreibungen zu verwenden. Soweit nicht objektiv unmöglich, müssen die Beschreibungen sachlich und politisch neutral verfasst werden, ohne jegliche wertende Komponente.</p>
+     <h2>Durchsetzung</h2>
+     <p>Wer gegen diesen Code of Conduct verstößt, kann zeitweise oder dauerhaft von der Nutzung der Plattform ausgeschlossen werden.</p>
+     <p>Wenn du einen Begriff siehst, der gegen diesen CoC verstößt, kannst du ihn <em>melden</em>. Dann wird sich unser Team um die Entfernung des Begriffs kümmern.</p>
+</main>
+</div>
+<footer class="footer">
+     <div class="container">
+       <span class="text-muted">Jugend hackt 2019, Berlin</span>
+       <span class="text-muted">Impressum &middot; &copy; Domain by Clemens Rau &middot; Redaktion: Neele &middot; Version: <?php system("git rev-list --count HEAD") ?></span>
+
+     </div>
+   </footer>
+</body>
+</html>
