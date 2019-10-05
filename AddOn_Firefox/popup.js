546043a1bc3c4eb03104142889e3525da4ff4b7d
zoxuyu <maus@paulstrobach.de>
2019-10-05 13:24:29 +0200

    Fehlermeldung, wenn kein Begriff gewählt

--- AddOn_Firefox/popup.js
+++ AddOn_Firefox/popup.js
@@ -1,1 +1,12 @@
-document.getElementById("term").innerText = window.localStorage["curia::selectedTerm"]; //request.selectedMessage;
+term = document.getElementById("term");
+description = document.getElementById("description")
+
+if (window.localStorage["curia::selectedTerm"] && window.localStorage["curia::selectedTerm"] != "undefined") {
+    term.innerText = window.localStorage["curia::selectedTerm"];
+    term.classList.remove("is-danger");
+    window.localStorage["curia::selectedTerm"] = undefined;
+} else {
+    term.innerText = "Kein Begriff gewählt";
+    term.classList.add("is-danger");
+    description.innerHTML = "<p>Du hast noch keinen Begriff gewählt, den CURIA erklären soll. Markiere einen Text auf der Seite und wähle im Kontextmenü (Rechte Maustaste) den Punkt <em>Begriff mit CURIA erklären</em>.</p>";
+}
