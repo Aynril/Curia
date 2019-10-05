e6d157f28b89b3bea9a56994022ceb0de818fafc
Clemens <kontakt@clemensrau.de>
2019-10-05 20:32:22 +0200

    test

--- Server_PHP/input.php
+++ Server_PHP/input.php
@@ -14,8 +14,5 @@ $kategorie = htmlspecialchars($_POST['kategorie']);
 $statement = $connection->prepare("INSERT INTO `begriffe` (`id`, `begriff`, `definition`, `aktiv`, `zuletzt_aktualisiert`, `autor`, `kategorie`) VALUES (NULL, '$begriff', '$definition', '1', CURRENT_TIMESTAMP, '$autor', '$kategorie')");
 $res = $statement->execute();
 
-<<<<<<< HEAD
-header("Location: index.html?info=success");
-=======
-echo "Successfully saved";
->>>>>>> c4579961c7614e9bab99044ec502454acd4783bd
+header("Location: ../index.html?info=success");
+
