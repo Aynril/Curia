223c5cfd0256d1a06827de485005ee6630a9ab02
Aynril <fabian.jaser@gmail.com>
2019-10-05 19:12:54 +0200

    made it a little securer

--- Server_PHP/input.php
+++ Server_PHP/input.php
@@ -3,11 +3,12 @@
 require("database.php");
 // This file enters the submitted data to the db
 
-$begriff = $_POST['begriff'];
-$definition = $_POST['definition'];
+
+$begriff = htmlspecialchar($_POST['begriff']);
+$definition = htmlspecialchar($_POST['definition']);
 $aktiv = 1;
-$zuletzt_aktualisiert = $_POST['zuletzt_aktualisiert'];
-$autor = $_POST['autor'];
-$kategorie = $_POST['kategorie'];
+$zuletzt_aktualisiert = htmlspecialchar($_POST['zuletzt_aktualisiert']);
+$autor = htmlspecialchar($_POST['autor']);
+$kategorie = htmlspecialchar($_POST['kategorie']);
 
 echo $begriff.$definition.$aktiv.$zuletzt_aktualisiert.$autor.$kategorie;
