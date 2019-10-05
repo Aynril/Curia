4bab64bde4cc2136ec2e982ea62504c762c05f9a
Clemens <kontakt@clemensrau.de>
2019-10-05 21:19:49 +0200

    tests

--- index.php
+++ index.php
@@ -16,25 +16,6 @@
   <!-- Brand / Logo-->
   <a class="navbar-brand" href="#">CURIA</a>
 
-  <!-- Toggler Button -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <!-- Navbar links -->
-  <div class="collapse navbar-collapse" id="collapsibleNavbar">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="#">Admin Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-    </ul>
-  </div>
 </nav> 
 
     <!-- Begin page content -->
