ae491d929f6b45b1c0377d651beab94d5949cc69
Clemens <kontakt@clemensrau.de>
2019-10-05 21:18:51 +0200

    tests

--- index.php
+++ index.php
@@ -12,20 +12,30 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-      <a class="navbar-brand" href="#">Curia</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <!-- Brand / Logo-->
+  <a class="navbar-brand" href="#">CURIA</a>
 
-        <ul class="nav navbar-nav navbar-right">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Admin Login <span class="sr-only"></span></a>
+  <!-- Toggler Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Navbar links -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Admin Login</a>
       </li>
-        </ul>
-      </div>
-    </nav>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+    </ul>
+  </div>
+</nav> 
 
     <!-- Begin page content -->
     <main role="main" class="container">
