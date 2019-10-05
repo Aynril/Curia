730ccbf17440f61e6807f57929559fe0c5b7d120
Aynril <fabian.jaser@gmail.com>
2019-10-05 22:52:52 +0200

    Added admin page

---
+++ admin.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="admin.css" ref=stylesheet>
+    <title>Just admin things</title>
+</head>
+<body>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Begriff</th>
+        <th>Definition</th>
+        <th>Kategorie</th>
+        <th>Autor</th>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <td colspan="3"> Diese Fußzeile erstreckt sich über die ganze Tabelle. </td>
+    </tr>
+</table>
+</body>
+</html>
