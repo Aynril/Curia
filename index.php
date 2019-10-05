845d616bb027fccd6389adda035b3c18133185ee
Clemens <kontakt@clemensrau.de>
2019-10-05 21:52:06 +0200

    tests

--- index.php
+++ index.php
@@ -17,6 +17,10 @@
   <!-- Brand / Logo-->
   <a class="navbar-brand" href="#">CURIA</a>
 
+  <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
 </nav> 
 
     <!-- Begin page content -->
